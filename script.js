04dd04ee4d74dd0cf9b2fcd6ebdd249efd3926f1
Inno Paolo <102833497+innopaolo@users.noreply.github.com>
2023-08-04 15:44:38 +0600

    Fix updateScreen function to add numbers consecutively within screen width

--- script.js
+++ script.js
@@ -5,34 +5,45 @@ let screenOp = document.querySelector("#screenOp");
 let numbers = document.querySelectorAll(".num")
 
 numbers.forEach(key => {
-    key.addEventListener("click", updateScreen)
+    key.addEventListener("click", (event) => updateScreen(event, key.id))
 })
 
-function updateScreen(event) {
-    screen.textContent = event.target.textContent;
-}
+function updateScreen(event, id) {
+    let currentContent = screen.textContent;
+    let newContent =  id;
 
+    let maxWidth = 10;
+
+    // Check if another key is pressed when "80085" is already displayed
+    if (currentContent === "8 0 0 8 5") {
+        // Replace "80085" with the new number
+        screen.textContent = newContent;
+    } else if (currentContent.length <= maxWidth) {
+        // Append the new number to the existing content
+        screen.textContent += newContent;
+    }
+}
 
 // Update screen with operators
 let operators = document.querySelectorAll(".op");
 
 operators.forEach(key => {
-    key.addEventListener("click", updateScreenOp)
+    key.addEventListener("click", (event) => updateScreenOp(event, key.id))
 })
 
-function updateScreenOp(event) {
-    screenOp.textContent = " " + event.target.textContent;
+function updateScreenOp(event, id) {
+    screenOp.textContent = id;
 }
 
 // Add functionality to special keys
 let specialKeys = document.querySelectorAll(".special");
 
 specialKeys.forEach(key => {
-    key.addEventListener("click", updateSpecial)
+    key.addEventListener("click", (event) => updateSpecial(event, key.id))
 })
 
-function updateSpecial(event) {
-    switch (event.target.textContent) {
+function updateSpecial(event, id) {
+    switch (id) {
         case "AC":
             screen.textContent = "";
             screenOp.textContent = "";
