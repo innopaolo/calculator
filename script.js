8ae0d8582ba3b64ddf778564960e5c1701deb402
Inno Paolo <102833497+innopaolo@users.noreply.github.com>
2023-08-04 14:20:19 +0600

    Add preliminary functions/framework for special keys

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let screen = document.querySelector("#screen");
+let screenOp = document.querySelector("#screenOp");
+
 // Update screen with numbers
 let numbers = document.querySelectorAll(".num")
 
@@ -6,7 +9,6 @@ numbers.forEach(key => {
 })
 
 function updateScreen(event) {
-    let screen = document.querySelector("#screen");
     screen.textContent = event.target.textContent;
 }
 
@@ -19,7 +21,34 @@ operators.forEach(key => {
 })
 
 function updateScreenOp(event) {
-    let screenOp = document.querySelector("#screenOp");
     screenOp.textContent = " " + event.target.textContent;
 }
 
+// Add functionality to special keys
+let specialKeys = document.querySelectorAll(".special");
+
+specialKeys.forEach(key => {
+    key.addEventListener("click", updateSpecial)
+})
+
+function updateSpecial(event) {
+    switch (event.target.textContent) {
+        case "AC":
+            screen.textContent = "";
+            screenOp.textContent = "";
+            break;
+        case "C":
+            // Code for "C" case 
+            break;
+        case "?":
+            screen.textContent = "8 0 0 8 5";
+            break;
+        case "+/-":
+            let array = screen.textContent[0];
+            console.log(array);  
+            break;
+        case "=":
+            // Code for "=" case 
+            break;
+    }
+}
